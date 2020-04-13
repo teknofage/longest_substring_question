9985149cdd9eabec83f8089f06a8be63158b7b90
teknofage <teknofage@gmail.com>
2020-04-13 13:35:54 -0700

    first commit

---
+++ longest_substring.py
@@ -0,0 +1,58 @@
+"""
+example input: “abcdaxyz”
+list of possible substrings:
+"a"
+"ab"
+"abc"
+"abcd"
+"b"
+"bc"
+"bcd"
+"bcda"
+"bcdax"
+"bcdaxy"
+"bcdaxyz" <---
+"c"
+"cd"
+"cda"
+"cdax"
+"cdaxy"
+"cdaxyz"
+"d"
+"da"
+"dax"
+"daxy"
+"daxyz"
+"ax"
+"axy"
+"axyz"
+"x"
+"xy"
+"xyz"
+"y"
+"yz"
+"z"
+
+"""
+
+
+
+first_letter = ""
+
+def find_longest(my_string):
+	my_string = transfer into a list
+    longest_sub = ""
+    for first_letter:
+        for last_letter:
+            check if duplicates in substring
+            check length of substring
+
+            if duplicates:
+            ignore
+            for substring 
+            if len > len(longest_sub):
+		        longest_sub = substring
+
+    if first_letter ==
+
+    return longest_sub
